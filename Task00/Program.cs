eb2d62bc0bf45c4f3061316721a91c0a110745be
Foat Khisamov <foat.com>
2023-04-22 13:03:09 +1000

    Добавили файл  Task05

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. на входе принимаут число и
+// 2. выдаёт аго квадрат (число умноженное на само себя)
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+// -2 147 483 648 ... 2 147 483 647
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на входе принимает два
+// числа и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите значение А");
+int numbersA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение B");
+int numbersB = Convert.ToInt32(Console.ReadLine());
+if (numbersA == numbersB * numbersB)
+{
+    Console.WriteLine("$Да, число {numberA} является квадратом числа {numberB} ");
+}
+else
+{
+     Console.WriteLine("$Нет, число {numberA} не является квадратом числа {numberB} ");
+}
+    
