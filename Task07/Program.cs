a59423606d154ea0529f3994651d7f55f479d7aa
Foat Khisamov <foat.com>
2023-04-22 14:03:13 +1000

    Добавили файл  Task06/

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход 
+// трёхзрачное число и выход показывает последнюю цифру
+//этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+Console.WriteLine("Ввидите трёхзначное число!");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>0) num *= -1;
+if (num>=100&&num<=999)
+{
+int lastNum = num % 10;
+Console.WriteLine($"Последняя цифра числа {num} => {lastNum}");
+}
+else Console.WriteLine("Некорректный ввод!");
