42fbd3bbe0d0c6745332ff3ceea9f15e66af6b59
Foat Khisamov <foat.com>
2023-04-23 16:36:51 +1000

    решили задачу в файле Task02/Program.cs

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,7 +1,20 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее,
-// а какое меньшее.
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее
+// и какое число меньшее?
 //Например: 
 // a = 5; b = 7 -> max = 7
 // a = 2; b = 10 -> max = 10
 // a = -9; b = -3 -> max = -3
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введи число a!");
+int numbersa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число b!");
+int numbersb = Convert.ToInt32(Console.ReadLine());
+int max = numbersa;
+int min = numbersa;
+if (numbersb > max) max = numbersb;
+{
+    Console.WriteLine($"Максимальное число является {max}!");
+}
+if (numbersb < min) min = numbersb;
+{
+    Console.WriteLine($"Минимальное число является {min}!");
+}
