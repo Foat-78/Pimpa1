9f8e10b82541130a7cd321f747856148c2521f0f
Foat Khisamov <foat.com>
2023-04-23 19:05:38 +1000

    Pешил задачу в файле Task08/Program.cs

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -3,4 +3,11 @@
 // Например:
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ввидите целое положительное число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= numbers)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
