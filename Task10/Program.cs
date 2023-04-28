642ec8ad78929e71416f059061bc324e6f047e73
Foat Khisamov <foat.com>
2023-04-28 22:05:47 +1000

    Решил задачу в файле Task10/Program.cs

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,3 +4,14 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = DispleySecondDigit(number);
+Console.WriteLine(result);
+int DispleySecondDigit (int num)
+{
+    int firstDigit = num / 10;
+    int thirdDigit = firstDigit % 10;
+    return thirdDigit;
+}
