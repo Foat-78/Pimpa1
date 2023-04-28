ca6d167d47b3517ead9696cbe5b71c618ba9b3c7
Foat Khisamov <foat.com>
2023-04-28 21:37:44 +1000

    Добавили задачу

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -4,3 +4,16 @@
 // второму, то программа выводит остаток от деления.
 // 34, 5 -> не кратно, остаток 4
 // 16, 4 -> кратно
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int remainder = Remainder(number1, number2);
+
+Console.WriteLine(remainder !=0 ? $"нет, остаток {remainder}" : "да");
+
+int Remainder(int num1, int num2)
+{
+    return num1 % num2;
+}
