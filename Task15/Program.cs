c30d8ce68a4906b29fe5ba544d69b5f3ce0b9e79
Foat Khisamov <foat.com>
2023-04-30 11:49:52 +1000

    Pешили задачу в файле Task15/Program.cs

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -4,3 +4,13 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+Console.WriteLine("Введите число недели для определения выходного дня");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = WallStreet(number);
+Console.WriteLine (result ? "да, это выходной" : "нет, не выходной");
+
+bool WallStreet(int num)
+{
+    return num == 7 || num == 6;
+}
