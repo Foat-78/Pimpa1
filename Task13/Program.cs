2b95b57b9d217c9b3f3ed0abb164e1425195e7d4
Foat Khisamov <foat.com>
2023-04-30 12:29:34 +1000

    не смог решить задачу в файле Task13/Program.cs

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,3 +4,17 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = DispleySecondDigit(number);
+Console.WriteLine(result);
+
+int DispleySecondDigit (int number)
+{
+    while (number > 999)
+    {
+        number /=10;
+    }
+    return number % 10;
+}
