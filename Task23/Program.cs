2f233cf7763ccc6923edfff8f55a7897482c1102
Foat Khisamov <foat.com>
2023-05-01 18:10:02 +1000

    Решил задачу в файле  Task23/Program.cs

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -2,3 +2,17 @@
 // 2. и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число N = ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Square(numberN);
+
+void Square(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($" Куб числа {count} ->  {count*count*count}");
+        count++;
+    }
+}
