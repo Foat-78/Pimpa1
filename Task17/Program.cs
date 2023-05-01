a5721e8ed7b4042502451c27676982ee861f8e0c
Foat Khisamov <foat.com>
2023-05-01 10:35:13 +1000

    Добавил файл  Task17/Program.cs

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -3,21 +3,22 @@
 // 3. выдаёт номер четверти плоскости, в которой находится эта точка.
 
 Console.WriteLine("Введите координаты точки");
-Console.WriteLine("X: ");
+Console.Write("X: ");
 int xCoordinate = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Y: ");
+Console.Write("Y: ");
 int yCoordinate = Convert.ToInt32(Console.ReadLine());
 
-int quarter = Quarter(xCoordinate, yCoordinate);
+int quarter = Quarter (xCoordinate, yCoordinate);
 string result = quarter > 0
-               ? $"Указанные координаты соответствует четверти -> {quarter}"
+                ? $"Указанные координаты соответствует четверти -> {quarter}"
                 : "Введены не корректные координаты";
 Console.WriteLine(result);
 
-int Quarter(int x, int y);
+int Quarter (int x, int y);
 {
     if (x > 0 && y > 0) return 1;
     if (x < 0 && y > 0) return 2;
     if (x > 0 && y < 0) return 3;
     if (x < 0 && y < 0) return 4;
+    return 0;
 }
