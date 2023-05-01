fe2fd70fa9aaf29a5cd8753fd7a2dde7a3add597
Foat Khisamov <foat.com>
2023-05-01 17:25:32 +1000

    Добавили файл и решили задачу в файле  Task22/

---
+++ Task22/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Напишите программу, которая принимает на вход число (N)
+// 2. и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите число N = ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Square(numberN);
+
+void Square(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($" Квадрат числа {count} ->  {count*count}");
+        count++;
+    }
+}
