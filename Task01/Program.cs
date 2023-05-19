be72b7383efdf91d8d1e79319b65c29fcc83422e
Foat Khisamov <foat.com>
2023-05-19 10:25:00 +1000

    исправил задачу в файле Task01/Program.cs

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -7,10 +7,10 @@ Console.WriteLine("Введите значение B");
 int numbersB = Convert.ToInt32(Console.ReadLine());
 if (numbersA == numbersB * numbersB)
 {
-    Console.WriteLine("$Да, число {numberA} является квадратом числа {numberB} ");
+    Console.WriteLine($"Да, число {numbersA} является квадратом числа {numbersB} ");
 }
 else
 {
-     Console.WriteLine("$Нет, число {numberA} не является квадратом числа {numberB} ");
+     Console.WriteLine($"Нет, число {numbersA} не является квадратом числа {numbersB} ");
 }
-    
+
