22fd1ce3dda881d2316d8a32c65385741fd174c2
Foat Khisamov <foat.com>
2023-05-19 10:29:03 +1000

    добавил задачу и рещил задачу в файле Task28/

---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = CountNumbers(number);
+
+Console.WriteLine($"Кол-во цифр в числе {number} =  {count}");
+
+
+int CountNumbers (int num)
+{
+    int i = 0;
+    while (num != 0) 
+    {
+        num = num / 10;
+        i++;
+    }
+    return i;
+}
