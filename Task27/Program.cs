a9ddedcd6c925ebc0bfa8bb642df4c1fdaa6a62e
Foat Khisamov <foat.com>
2023-05-21 08:19:39 +1000

    Решил домашнее задание в файле Task27/Program.cs

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -5,4 +5,20 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.Write("Введите число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
 
+int sumNumbers = SumNumbers(numbers);
+Console.WriteLine($"Сумма цифр {numbers} -> {sumNumbers}");
+
+int SumNumbers(int num)
+{
+   int sum = 0;
+    while (num > 0)
+    {
+       sum += num % 10;
+       num = num / 10;
+       
+    }        
+        return sum;
+}
