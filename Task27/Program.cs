baaad441c2c21a372a5a38c9bf71c4f0d2c28219
Foat Khisamov <foat.com>
2023-05-21 11:02:33 +1000

    Добавил коммит

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -16,9 +16,8 @@ int SumNumbers(int num)
    int sum = 0;
     while (num > 0)
     {
-       sum += num % 10;
-       num = num / 10;
-       
+       sum = sum + num % 10; //эквивалент sum += num % 10;
+       num = num / 10;            
     }        
         return sum;
 }
