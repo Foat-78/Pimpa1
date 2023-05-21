26488b106b19fcc8f2843bc5c5f7759b9cf74b61
Foat Khisamov <foat.com>
2023-05-21 14:22:31 +1000

    Решил задачу в файле Task25/Program.cs

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -3,3 +3,24 @@
 //  и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exponentiationNumber = ExponentiationNumber(numberA, numberB);
+Console.WriteLine($"{numberA}, {numberB} -> {exponentiationNumber}");
+
+int ExponentiationNumber(int numA, int numB)
+{
+    int num = 1;
+    int i = 0;
+    while (i < numB)
+    {
+        num = num * numA;
+        i++;
+    }
+    return num;
+}
+
