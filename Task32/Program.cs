733108a0ad8daafab48789d50118a83e8189871c
Foat Khisamov <foat.com>
2023-05-23 04:14:08 +1000

    Решил задачу в файле  Task32/

---
+++ Task32/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+void InverseArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+
+Console.WriteLine ("Введите число элементов");
+int elements = Convert.ToInt32(Console.ReadLine());
+int [] arr = new int [elements];
+
+Console.WriteLine ("Введите минимальное число диапазона");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите максимальное число диапазона");
+int maximum = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayRndInt(elements, minimum, maximum+1);
+
+
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine("]");
+
+InverseArray (array);
+
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine("]");
