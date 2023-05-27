91a708ddf870a7998eb908ad960f22ef22cd717c
Foat Khisamov <foat.com>
2023-05-27 18:50:19 +1000

    Решил домашнее задание в файле Task34/Program.cs

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -2,3 +2,41 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max) 
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int EvenNumbers(int[] arr)
+{
+    int num = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0) num++;
+    }
+    return num;
+}
+
+int[] array = CreateArrayRndInt(6, 100, 999);
+Console.Write("[");
+PrintArray(array, ",");
+Console.Write($"] -> ");
+
+int evenNumbers = EvenNumbers(array);
+Console.WriteLine($"{evenNumbers}");
