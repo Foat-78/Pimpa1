ad0331c591aabbd60c46fb098d16648de6d9c43e
Foat Khisamov <foat.com>
2023-05-27 10:32:49 +1000

    Решил задание через цикл for  Task28/

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -10,18 +10,28 @@ int number = Convert.ToInt32(Console.ReadLine());
 int produktNumber = ProduktNumber(number);
 Console.WriteLine($"Кол-во цифр в числе {number} =  {produktNumber}");
 
-int ProduktNumber (int num)
-{
-    int count = 1;
-    int  i = 1;
-    while (i <= num) 
-    {
-        count *= i;
-        i++;
-    }
-    return count;
-}
+// int ProduktNumber (int num)
+// {
+//     int count = 1;
+//     int  i = 1;
+//     while (i <= num) 
+//     {
+//         count *= i;
+//         i++;
+//     }
+//     return count;
+// }
 //1*1=1
 //1*2=2
 //2*3=6
 //6*4=24
+
+int ProduktNumber (int num)
+{
+    int count = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        count *= i;
+    }
+    return count;
+}
