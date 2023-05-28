769f2e5378144a8d2eec52ca76862961ea8984ae
Foat Khisamov <foat.com>
2023-05-28 12:44:17 +1000

    Pешил домашнее задание в файле Task36/Program.cs

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -5,3 +5,46 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int SumNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int[] array = CreateArrayRndInt(6, -10, 99);
+Console.Write("[");
+PrintArray(array, ",");
+Console.Write($"] -> ");
+
+int sumNumbers = SumNumbers(array);
+Console.WriteLine($"{sumNumbers}");
+
+// Ууууууууууррррррррррррраааааааа!!!!!!
