86d07fd76b9dc231e2a22998802e0f2b9cd121fa
Foat Khisamov <foat.com>
2023-05-28 19:24:55 +1000

    Решил домашнее задание в файле Task38/Program.cs - УУУУрррраааа!!!

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -4,14 +4,11 @@
 
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
-
-
-
 double[] CreateArrayRndDouble(int size, int min, int max)
 {
     double[] arr = new double[size];
     Random rnd = new Random();
-
+    
     for (int i = 0; i < arr.Length; i++)
     {
         double num = rnd.NextDouble() * (max - min) + min;
@@ -27,3 +24,33 @@ void PrintArrayDouble(double[] arr, string sep)
         else Console.Write($"{arr[i]}");
     }
 }
+
+double MinElem(double[] arr)
+{
+    double min = arr[0];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+double MaxElem(double[] arr)
+{
+    double max = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+
+double[] array = CreateArrayRndDouble(5, 0, 100);
+Console.Write("[");
+PrintArrayDouble(array, ";");
+Console.Write("] -> " );
+
+double differenceElements = MaxElem(array) - MinElem(array);
+Console.Write($"{Math.Round(differenceElements, 1)}");
