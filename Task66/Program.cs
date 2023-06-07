792afd59b33e680a4c22b3fde53409e71258587f
Foat Khisamov <foat.com>
2023-06-07 13:20:18 +1000

    Решил ДЗ в файл Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -11,21 +11,19 @@ int numberM = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите натуральное число N: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 
-int naturalNamberRange = NaturalNamberRange(numberM, numberN);
-Console.WriteLine($"{naturalNamberRange}");
+if (numberM < 0 || numberN < 0) Console.Write($"Ошибка - введите натуральное число");
+else
+{
+    int naturalNamberRange = NaturalNamberRange(numberM, numberN);
+    Console.WriteLine($"M = {numberM}; N = {numberN} -> {naturalNamberRange}");
+}
 
 int NaturalNamberRange(int numM, int numN)
 {
-    if (numM < 0 && numN < 0) Console.Write($"Ошибка - введите натуральное число");
-    else
+    if (numM == numN) return numN;
+    else if (numM > numN)
     {
-        if (numM == numN) return numN;
-
-        else if (numM > numN)
-        {
-            return numM + NaturalNamberRange(numM - 1, numN);
-        }
-        return numN + NaturalNamberRange(numM, numN - 1);//5, 4, 3, 2, 1
+        return numM + NaturalNamberRange(numM - 1, numN);
     }
-    return numM;
+    return numN + NaturalNamberRange(numM, numN - 1);//5, 4, 3, 2, 1
 }
